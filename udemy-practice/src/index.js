3fe807cf0a68cd43cb2239e800ebfb73e07954b0
Andrew Koshelev <ndrei61999@gmail.com>
2021-02-19 05:45:17 -1000

    props

--- udemy-practice/src/index.js
+++ udemy-practice/src/index.js
@@ -1,15 +1,28 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './components/css/index.css'
 import './components/css/app.css'
-import App from './components/app/';
+// import App from './components/app/';
 // import reportWebVitals from './reportWebVitals';
 
 
 
-ReactDOM.render(<App />, document.getElementById('root')
+function WhoAmI(props) {
+    return (
+        // <React.Fragment>
+        <> 
+
+            <h1>My name is {props.name}, surname - {props.surname}</h1>
+            <a href={props.link}>My Profile</a>
+        </>
+        // </React.Fragment>
+    )
+}
+
+ReactDOM.render(<WhoAmI name='Meg' surname='Root' link='rewand.netlify.app' />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
